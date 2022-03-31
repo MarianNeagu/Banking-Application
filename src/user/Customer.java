5c956c2509f3ec94de21bcb206c2a48fa2fe9faf
Marian Neagu <marianmadalin01@gmail.com>
2022-04-01 00:40:57 +0300

    added files

---
+++ src/user/Customer.java
@@ -0,0 +1,87 @@
+package user;
+
+import java.util.Scanner;
+
+public class Customer extends User{
+
+    private int customerId;
+    private String firstName;
+    private String lastName;
+    private String CNP;
+    private String email;
+    private String phoneNumber;
+
+    public Customer(){
+
+    }
+
+    public Customer(int customerId, String firstName, String lastName, String CNP, String email, String phoneNumber) {
+        this.customerId = customerId;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.CNP = CNP;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public void readDetails(Scanner in){
+        System.out.println("First name: ");
+        this.firstName = in.nextLine();
+        System.out.println("Last name: ");
+        this.lastName = in.nextLine();
+        System.out.println("CNP: ");
+        this.CNP = in.nextLine();
+        System.out.println("Email address: ");
+        this.email = in.nextLine();
+        System.out.println("Phone number: ");
+        this.phoneNumber = in.nextLine();
+    }
+
+    public int getCustomerId() {
+        return customerId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getCNP() {
+        return CNP;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setCustomerId(int customerId) {
+        this.customerId = customerId;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setCNP(String CNP) {
+        this.CNP = CNP;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+}
